433d3ac6eb345b64ad39efbdc34e114fe56ea45a
Luke Singham <lukesingham@gmail.com>
2017-09-19 22:17:47 +0100

    Small tidy up of script

--- initial_setup.sh
+++ initial_setup.sh
@@ -8,11 +8,8 @@ sudo apt-get install vim
 # Install chrome
 # known issue: https://askubuntu.com/a/765039
 echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list  
-
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -  
-
 sudo apt-get update  
-
 sudo apt-get install google-chrome-stable
 
 # Install spotify
@@ -24,10 +21,10 @@ sudo apt-get update && sudo apt-get install spotify-client
 wget https://repo.skype.com/latest/skypeforlinux-64.deb
 sudo dpkg -i skypeforlinux-64.deb
 sudo apt install -f
+rm -f skypeforlinux-64.deb
 
 # Install TLP (battery mgmt for laptops)
 sudo add-apt-repository ppa:linrunner/tlp
 sudo apt update
 sudo apt install tlp tlp-rdw
-
 
