2ca475099aee2955937ab9d239ccda8d21360584
Luke Singham <lukesingham@gmail.com>
2017-09-20 09:41:03 +0100

    Add docker-ce install

--- initial_setup.sh
+++ initial_setup.sh
@@ -28,3 +28,9 @@ sudo add-apt-repository ppa:linrunner/tlp
 sudo apt update
 sudo apt install tlp tlp-rdw
 
+# Install Docker-ce
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
+sudo apt-get update
+sudo apt-get install docker-ce
