08d075f3601a2a87adb8b9cd434681b4dbdbac8b
Luke Singham <lukesingham@gmail.com>
2017-09-20 10:14:43 +0100

    Add pip install

--- initial_setup.sh
+++ initial_setup.sh
@@ -34,3 +34,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
 sudo apt-get update
 sudo apt-get install docker-ce
+
+# Install pip
+sudo apt install python-pip
